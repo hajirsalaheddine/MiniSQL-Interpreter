8e79d8de17090654bb26fc52f87fa68e28c2e02c
Hajir Salah Eddine <hrpokayolo@gmail.com>
2025-12-06 02:40:50 +0100

    Add files via upload

---
+++ MiniSQL-Interpreter/01_create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Client (
+    numClt INT,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    age INT
+);
---
+++ MiniSQL-Interpreter/05_delete_data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Client (
+    numClt INT,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    age INT
+);
+
+INSERT INTO Client VALUES (1, 'Mehdi', 'Jadallah', 35);
+INSERT INTO Client (numClt, nom) VALUES (2, 'Nasrallah');
+
+SELECT * FROM Client;
+SELECT nom, prenom FROM Client WHERE numClt = 2;
+SELECT * FROM Client WHERE age > 30 AND nom = 'Mehdi';
+
+UPDATE Client SET age = 36 WHERE numClt = 1;
+UPDATE Client SET nom = 'Saif', age = 40 WHERE numClt = 2;
+
+DELETE FROM Client WHERE age < 18;
+DELETE FROM Client;
