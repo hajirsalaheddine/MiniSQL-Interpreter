60b7af73284ebb5edce16d7e3d0d195873f133d2
Hajir Salah Eddine <hrpokayolo@gmail.com>
2025-12-06 02:33:56 +0100

    Delete MiniSQL-Interpreter directory

--- MiniSQL-Interpreter/01_create_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Client (
-    numClt INT,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    age INT
-);
--- MiniSQL-Interpreter/02_insert_data.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Client (
-    numClt INT,
-    nom VARCHAR(50),
-    prenom VARCHAR(50),
-    age INT
-);
-
-INSERT INTO Client VALUES (1, 'Alaa', 'Jadallah', 35);
-INSERT INTO Client (numClt, nom) VALUES (2, 'Nasrallah');
